73bc6b2e27dfd407fc65e06916b971fcc481e17b
lijiayuan <lijiayuan365@gmail.com>
2024-11-12 09:32:18 +0800

    feat: 9898 - Appear only once

---
+++ 09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+import { ExpectFalse, NotEqual } from './test-utils'
+
+type cases = [
+  Expect<Equal<FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 6]>, [1, 4, 5]>>,
+  Expect<Equal<FindEles<[2, 2, 3, 3, 6, 6, 6]>, []>>,
+  Expect<Equal<FindEles<[1, 2, 3]>, [1, 2, 3]>>,
+]
+
+
+// ============= Your Code Here =============
+type FindEles<T extends any[], Duplicates = never> = T extends [
+  infer F,
+  ...infer R
+]
+  ? F extends Duplicates
+  ? FindEles<R, Duplicates>
+  : F extends R[number]
+  ? FindEles<R, Duplicates | F>
+  : [F, ...FindEles<R, Duplicates>]
+  : [];
